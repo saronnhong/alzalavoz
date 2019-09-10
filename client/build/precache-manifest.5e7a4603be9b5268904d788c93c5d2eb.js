7dc7702c0c049b6fc4ce323470aa117785801438
Saron <saronnhong@gmail.com>
2019-09-10 10:09:52 -0700

    build created for heroku

---
+++ client/build/precache-manifest.5e7a4603be9b5268904d788c93c5d2eb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb599f502b992bdd75b52e11cfcd3734",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2421d38e894db18fd2f9",
+    "url": "/static/css/main.08899707.chunk.css"
+  },
+  {
+    "revision": "fa97bc0ff139572dcc71",
+    "url": "/static/js/2.e7c9e8c7.chunk.js"
+  },
+  {
+    "revision": "2421d38e894db18fd2f9",
+    "url": "/static/js/main.300985b1.chunk.js"
+  },
+  {
+    "revision": "37facd21dbf1673402a7",
+    "url": "/static/js/runtime~main.ce49a5c8.js"
+  }
+]);
